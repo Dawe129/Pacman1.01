367ca174ac483b61d0d86c74234da13d684120f4
Dawe129 <dpivonka1@gmail.com>
2024-05-18 13:55:29 +0200

    Nastaveni vlastnosti pro pacmana a duchy + kostra metod pro pohyb a kresleni

--- src/Duch.java
+++ src/Duch.java
@@ -1,2 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Duch {
+    private int x, y;
+    private int velikost = 20;
+
+    public Duch(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    public void Pohyb(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void Kresli(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, velikost, velikost);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getVelikost() {
+        return velikost;
+    }
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -1,2 +1,68 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Pacman {
+    private int x, y;
+    private int velikost = 20;
+    private int dx, dy;
+    private int rychlost = 5;
+    private int skore;
+
+    public Pacman(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+        this.dx = 0;
+        this.dy = 0;
+        this.skore = 0;
+    }
+
+    public void Pohyb() {
+        this.x += dx * rychlost;
+        this.y += dy * rychlost;
+    }
+
+    public void nastavSmer(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void pridejSkore(int body) {
+        this.skore += body;
+    }
+
+    public int getSkore() {
+        return this.skore;
+    }
+
+    public void Kresli(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillArc(x, y, velikost, velikost, 45, 270);
+    }
+
+    public boolean Kolize(Duch duch) {
+        return this.x < duch.getX() + duch.getVelikost() &&
+                this.x + velikost > duch.getX() &&
+                this.y < duch.getY() + duch.getVelikost() &&
+                this.y + velikost > duch.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getVelikost() {
+        return velikost;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
