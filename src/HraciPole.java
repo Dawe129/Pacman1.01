aca4334e918d0987a2543324a5fd996471fb4e67
Dawe129 <dpivonka1@gmail.com>
2024-05-18 15:11:38 +0200

    ve tride HraciPole byly pridany metody na sirkou, vysku a velikost pole, metoda na okraj pole + random steny uprostred pole a metoda na vypsani pole

--- src/HraciPole.java
+++ src/HraciPole.java
@@ -1,3 +1,66 @@
-public class HraciPole {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class HraciPole extends JPanel {
+    private char[][] pole;
+    private int sirka;
+    private int vyska;
+    private int velikostPolicka;
+
+    public HraciPole(int sirka, int vyska, int velikostPolicka) {
+        this.sirka = sirka;
+        this.vyska = vyska;
+        this.velikostPolicka = velikostPolicka;
+        this.pole = new char[vyska][sirka];
+        inicializujPole();
+    }
+
+    private void inicializujPole() {
+        for (int i = 0; i < vyska; i++) {
+            for (int j = 0; j < sirka; j++) {
+                if (i == 0 || i == vyska - 1 || j == 0 || j == sirka - 1) {
+                    pole[i][j] = '#';
+                } else {
+                    pole[i][j] = '.';
+                }
+            }
+        }
+
+        Random random = new Random();
+        int pocetSten = sirka * vyska / 10;
+        for (int k = 0; k < pocetSten; k++) {
+            int x = random.nextInt(sirka - 2) + 1;
+            int y = random.nextInt(vyska - 2) + 1;
+            if (pole[y][x] != '#') {
+                pole[y][x] = '#';
+            }
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 0; i < vyska; i++) {
+            for (int j = 0; j < sirka; j++) {
+                if (pole[i][j] == '#') {
+                    g.setColor(Color.BLUE);
+                    g.fillRect(j * velikostPolicka, i * velikostPolicka, velikostPolicka, velikostPolicka);
+                } else if (pole[i][j] == ' ') {
+                    g.setColor(Color.BLACK);
+                    g.fillRect(j * velikostPolicka, i * velikostPolicka, velikostPolicka, velikostPolicka);
+                }
+            }
+        }
+    }
+
+    public void aktualizujPole(char[][] novePole) {
+        this.pole = novePole;
+        repaint();
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(sirka * velikostPolicka, vyska * velikostPolicka);
+    }
 
 }
